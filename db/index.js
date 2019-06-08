7c71cdf74c3bc76362453fdead5eb5eb5359891d
Mrinal <dmrinal626@gmail.com>
2019-06-08 15:36:16 +0530

    removed node_modules and put it in gitignore

--- db/index.js
+++ db/index.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    user: "dbaybxtynyshgf",
-    password: "2efe491307c4c9b6275eb7cedd285b79508d265c25ac9dcbb954713424ca404a",
-    database: "dnvirmkujfnvj",
+    user: "username",
+    password: "password",
+    database: "database",
     port: 5432,
-    host: "ec2-184-72-238-22.compute-1.amazonaws.com",
+    host: "hostname",
     ssl: true
 }); 
 module.exports = {
